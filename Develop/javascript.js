a1c4b5a77a2ed5bbb18fa6455042b3f32124986c
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-02-22 10:32:02 -0500

    added js variables to specify to each html element added for each hour slot, and #currentDay in header

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,1 +1,12 @@
 //here is the javascript file
+var WholeBlock = document.querySelectorAll(".Timeblocks");
+var nineAm = document.querySelector("#9");
+var tenAm = document.querySelector("#10");
+var elevenAm = document.querySelector("#11");
+var twelveAm = document.querySelector("#12");
+var oneAm = document.querySelector("#1");
+var twoAm = document.querySelector("#2");
+var threeAm = document.querySelector("#3");
+var fourAm = document.querySelector("#4");
+var fiveAm = document.querySelector("#5");
+var currentDay = document.querySelector("#currentDay");
