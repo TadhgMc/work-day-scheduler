23a02f89904f470587200758f71443a3110b1121
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-02-24 12:48:43 -0500

    added comments to js to explain what each part is doing, took out console.logs that were used for testing purposes and any unnecessary comments used as personal notes

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,7 +1,9 @@
-//here is the javascript file
-var Container = $(".container");
+// creating variables that point to different divs in html
+//these 2 are used for tracking day and time
 var WholeBlock = $(".Timeblock");
+var currentDay = $("#currentDay");
 
+//these are to indicate to each div that includes all elements for each timeblock
 var nineAm = $("#9");
 var tenAm = $("#10");
 var elevenAm = $("#11");
@@ -11,32 +13,40 @@ var fourteen = $("#14");
 var fifteen = $("#15");
 var sixteen = $("#16");
 var seventeen = $("#17");
-var currentDay = $("#currentDay");
 
 
-//dddd, MMMM Do YYYY moment format into a variable
+
+//set text at top of page that displays todays date
 var today = moment().format("dddd, MMMM Do YYYY");
 currentDay.text(today);
 
-
-console.log(WholeBlock.data("hour")); //to test calling data info
-
-//creating a variable that shows what hour it is as an integer
+//creating a variable that shows what hour it is as an integer, which will be used later to determine the color of each section
 var hour = parseInt(moment().format("H"));
-console.log(hour);//make sure correct hour is chosen
 
-// make each row run this function BlockColor
+
+
 //make each div variable in an array, then choose i for which one to call
 var AllBlock = [nineAm,tenAm,elevenAm,twelveAm,thirteen,fourteen,fifteen,sixteen,seventeen];
+
+/*for loop to set the color, add eventlistener to the button,
+set input text to anything saved in localstorage,
+and save input to localstorage when the add button is clicked*/
 
 for (var i = 0; i < AllBlock.length; i++) {
     BlockColor(AllBlock[i]);
 }
 
-//make an interval to check the time every 30 mins(?) then update the colors accordingly (?)
+/*here is function that is called in the for loop
+this section references the input from the "for" loop (AllBlock[1]) as "this"
+and makes the variable "x" point to whichever part of the array "AllBlocks" is referenced to by the "for" loop*/
 function BlockColor(x = this){
+    /*lines 47-56 finds the input element in each div referenced in the AllBlocks array,
+    then changes the background color to either red, blue or green depending if
+    the time has passed, it is currently that time, or it is still to come.
+    the function does this by referencing the data-number I gave to each div to specify which hour block it represents ("x.data("hour")")*/
     var input = $('input');
     var TaskInput = x.find(input);
+
     if (x.data("hour") === hour){
         TaskInput.addClass('bg-info');
     } else if (x.data("hour") > hour) {
@@ -45,35 +55,21 @@ function BlockColor(x = this){
         TaskInput.addClass('bg-danger');
     }
 
+    /*create variable to specify which time block the for loop is adding to, 
+    so as to specify in local storage where inputs are coming from, and where they will go*/
     var currentHour = x.data("hour");
     
+    //this gets any info saved to localstorage for this specific time block, and puts it into the input 
     TaskInput.val(localStorage.getItem(currentHour));
 
+    //create variables used to specify the button element in each time block
     var Btndiv = $('button');
-    var addBtn = x.find(Btndiv); 
+    var addBtn = x.find(Btndiv);
+
+    /*adds event listener to each button, that if the button is clicked,
+    save whatever is in the input area to the localstorage tied together with which timeblock it was saved from*/
     addBtn.on("click", function(){
-    console.log("savePlan func ");
      currentPlan = TaskInput.val();
-     console.log(currentPlan);
      localStorage.setItem(currentHour, currentPlan);
     });
-
-    
-    
-
 }
-
-//also going to need to add functionality, to display saved plans onto the planner from past sessions
-
-//for saving the inputs, only have whatever is in the box, be what is saved to the localstorage. no weird saving/populating
-// for loop to add click listener to all the buttons, then call savePlans function in the eventlistener
-
-
-//can the eventlistener just be added in the earlier for loop to add the listner to each button?
-
-
-
-// var savePlans = function() {
-    
-// }
-// MIGHT GENERALLY NEED TO BE MORE SPECIFIC WITH WHICH DIV WE ARE TARGETING FOR WHAT
